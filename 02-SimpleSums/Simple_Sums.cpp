6244dba8ab8780d38027b5bf73cb2cfedc49b763
stefanm1337 <stmiloiko@yahoo.com>
2025-07-27 09:53:18 +0100

    Add files via upload
    I declared 3 variables one with value 8 other with value 10 and the 3rd one to store the sum of them, and then I printed the sum to the console.

---
+++ 02-SimpleSums/Simple_Sums.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+
+int main(){ // main function
+    int a=8; // variable to store first number
+    int b=10; // variable to store second number
+    int c=a+b; // variable to store the sum of a and b
+    cout<<"The sum of "<<a<<" and "<<b<<" is "<<c<<endl; // prints the sum of a and b
+
+    return 0;
+}
