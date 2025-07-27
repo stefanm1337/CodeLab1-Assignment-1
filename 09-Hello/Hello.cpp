c2fcf62351c08f6532139d8f39bc0cc8d7aebb41
stefanm1337 <stmiloiko@yahoo.com>
2025-07-27 10:07:41 +0100

    Add files via upload
    I've filled in the blanks to create a function out of the main one that prints "Hello" to the console and called the function in the main one to do so.

---
+++ 09-Hello/Hello.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+void hello(){ // function to print "Hello"
+  cout << "Hello" << endl; // prints "Hello" to the console
+}
+
+int main(){ // main function to execute the program
+
+  hello(); // calling the hello function to print "Hello"
+
+  return 0;
+}
