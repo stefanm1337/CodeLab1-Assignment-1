51372ac8cad863ccb3756129bdf81c35e534c5c0
stefanm1337 <stmiloiko@yahoo.com>
2025-07-27 09:50:51 +0100

    Add files via upload
    I've filled the blank spaces in the code so the main function prints "Coding is cool" in the console

---
+++ 01-CodingIsCool/Coding_is_Cool.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() { // main function
+     cout<<"Coding is Cool"<< endl; // prints the message "Coding is Cool"
+
+     return 0;
+}
